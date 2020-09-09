1537edce101eb528d8e363f33185a0449e911376
Jorge Ramirez <jorgearmandoramirezrocha@gmail.com>
2020-09-08 20:39:10 -0600

    Se agrega el estandar de variables, y comentarios necesarios así como codigo funcional.

--- src/método/newton/raphson/MétodoNewtonRaphson.java
+++ src/método/newton/raphson/MétodoNewtonRaphson.java
@@ -13,31 +13,39 @@ public class MétodoNewtonRaphson {
         Scanner t = new Scanner(System.in);
         
         System.out.println("Introduce la formula a calcular: ");
-        String formula = t.nextLine();
+        String vlstrFormula = t.nextLine();
         System.out.println("Introduce el nivel de aproximación: ");
-        Integer nivel = t.nextInt();
+        Integer vlintNivel = t.nextInt();
         
-        System.out.println(numTerminos(formula));
+        System.out.println(numTerminos(vlstrFormula));
         
         
     }
     
+    //Se utiliza para revisar la cantidad de terminos en la formula brindada
+    //basandose en la cantidad de simbolos + y -
     public static int numTerminos(String formula){
-        int numeroDeTerminos = 0;
+        int vlintNumeroDeTerminos = 0;
+        boolean vlblnPrimerTerminoContado = false;
+        //Se considera el primer termino-
         if(formula.startsWith("+") || formula.startsWith("-")){
-            numeroDeTerminos = 1;
+            vlintNumeroDeTerminos = 1;
+            vlblnPrimerTerminoContado = true;
         }
-        
-        
+        boolean vlblnTerminoEncontrado = false;
         for (int n = formula.length()-1; n >= 0; n--) { 
             char c = formula.charAt (n); 
             if( c == '-' || c == '+'){
-                numeroDeTerminos++;
+                vlintNumeroDeTerminos++;
+                vlblnTerminoEncontrado = true;
             }
         }
-        
-        return numeroDeTerminos+1;
-        
+        if(vlblnPrimerTerminoContado){
+            return vlintNumeroDeTerminos-1;
+        } else {
+        //Se le suma uno para evitar que se ignore el primer termino
+        return vlintNumeroDeTerminos+1;
+        }
     }
     
     
